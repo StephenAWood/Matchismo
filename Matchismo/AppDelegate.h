3ece143f396959bb4293f256fdf123d0081a29cf
Stephen Wood <stephenwood@Stephens-MacBook-Pro.local>
2015-01-16 20:55:25 -0500

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Stephen Wood on 2015-01-16.
+//  Copyright (c) 2015 Stephen Wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Stephen Wood on 2015-01-16.
+//  Copyright (c) 2015 Stephen Wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
